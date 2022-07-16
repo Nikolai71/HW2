53cd528c10109612d13fcc1418bf1d678ca095bd
Nikolai <nikolai71@msn.com>
2022-07-16 20:23:14 +0300

    create ex1

--- ex1/Program.cs
+++ ex1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите трехзначное число.\nДля выхода из программы команда Exit.");
+    while(true)
+    {
+        string input = Console.ReadLine();
+        if(!input.Equals("exit"))
+            Console.WriteLine("{0}->{1}",input, input[1]);
+        else
+            break;
+    }
